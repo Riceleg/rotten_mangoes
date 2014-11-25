641b9e1c20333b956354462d32720543046bcc0a
Michael LeGrice <legrice@gmail.com>
2014-11-25 20:52:22 +0000

    created ratings & validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user.params)
+    @user = User.new(user_params)
 
     if @user.save
       session[:user_id] = @user.id #auto log in
