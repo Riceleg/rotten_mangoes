a3905b6814c3235e2c7a9f646fb7b86d2c32c557
Michael LeGrice <legrice@gmail.com>
2014-11-28 00:12:40 +0000

    added admin user powers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,14 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def admin_restrict
+    if !(current_user && current_user.admin)
+      flash[:alert] = "sign in as admin"
+      redirect_to movies_path
+    end
+  end
+  
+
   helper_method :current_user
 
 end
